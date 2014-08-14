65af2121a5c697588d92e6e4581fb1d3f5d515e7
yitznewton <yitznewton@hotmail.com>
2014-08-13 22:20:17 -0400

    refactor test

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,43 +1,44 @@
 require 'hand/one_pair'
 require 'hand/two_pairs'
 require 'hand/three_of_kind'
+require 'hand/straight'
 
-def cards(*codes)
+def cards(codes)
   codes.map { |c| Card.new(c) }
 end
 
 describe OnePair do
+  subject(:hand) { OnePair.new }
+
   it 'does not match a single card' do
-    hand = OnePair.new
-    expect(hand.matches? cards('2S')).to be false
+    expect(hand.matches? cards(%w(2S))).to be false
   end
 
   it 'matches a pair' do
-    hand = OnePair.new
-    expect(hand.matches? cards('2S', '2D')).to be true
+    expect(hand.matches? cards(%w(2S 2D))).to be true
   end
 end
 
 describe TwoPairs do
+  subject(:hand) { TwoPairs.new }
+
   it 'does not match a single pair' do
-    hand = TwoPairs.new
-    expect(hand.matches? cards('2S', '2D')).to be false
+    expect(hand.matches? cards(%w(2S, 2D))).to be false
   end
 
   it 'matches two pairs' do
-    hand = TwoPairs.new
-    expect(hand.matches? cards('2S', '2D', '4S', '4D')).to be true
+    expect(hand.matches? cards(%w(2S 2D 4S 4D))).to be true
   end
 end
 
 describe ThreeOfKind do
+  subject(:hand) { ThreeOfKind.new }
+
   it 'does not match two pairs' do
-    hand = ThreeOfKind.new
-    expect(hand.matches? cards('2S', '2D', '4S', '4D')).to be false
+    expect(hand.matches? cards(%w(2S 2D 4S 4D))).to be false
   end
 
   it 'matches three of a kind' do
-    hand = ThreeOfKind.new
-    expect(hand.matches? cards('2S', '2D', '2C')).to be true
+    expect(hand.matches? cards(%w(2S 2D 2C))).to be true
   end
 end
