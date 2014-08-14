8237b375607c38523bdc3a1aac632c3f2494734a
yitznewton <yitznewton@hotmail.com>
2014-08-14 11:43:09 -0400

    clean up a method

--- lib/hand_matcher/two_pairs.rb
+++ lib/hand_matcher/two_pairs.rb
@@ -1,6 +1,6 @@
 class TwoPairs
   def matches?(hand)
-    rank_counts = hand.cards.map(&:rank).group_by(&:to_s).values
+    rank_counts = hand.cards.group_by(&:rank).values
     pair_groups = rank_counts.select { |g| g.length >= 2 }
     pair_groups.length == 2
   end
