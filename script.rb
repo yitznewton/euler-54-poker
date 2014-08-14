0790660e6f87980705c24878b02b8eeee1542247
yitznewton <yitznewton@hotmail.com>
2014-08-14 11:18:12 -0400

    add script to evaluate wins in file

--- script.rb
+++ script.rb
@@ -1,4 +1,5 @@
 require_relative 'lib/card'
+require_relative 'lib/hand'
 require_relative 'lib/hand_matcher/two_pairs'
 require_relative 'lib/hand_matcher/straight'
 require_relative 'lib/hand_matcher/flush'
@@ -9,4 +10,16 @@ require_relative 'lib/hand_matcher/n_of_kind'
 require_relative 'lib/hand_ranker'
 
 ranker = HandRanker.new
+p1_wins = 0
+
+STDIN.read.split("\n").each do |line|
+  p1_hand = Hand.from_string(line[0, 14])
+  p2_hand = Hand.from_string(line[-15, 14])
+
+  if ranker.compare(p1_hand, p2_hand) == (-1)
+    p1_wins += 1
+  end
+end
+
+puts 'Player 1 won %d times' % p1_wins
 
